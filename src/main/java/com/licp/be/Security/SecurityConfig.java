1b22d5b56b69c2e927b98908dcc91e704924198a
Yee_dochi <ghksqkdwod0@gmail.com>
2025-09-01 12:17:42 +0900

    Security,index init

---
+++ src/main/java/com/licp/be/Security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.licp.be.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+        public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
+                http 
+                        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .formLogin(AbstractHttpConfigurer::disable)
+                        .httpBasic(AbstractHttpConfigurer::disable)
+                        .logout(AbstractHttpConfigurer::disable)
+                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authorizeHttpRequests(authorizeRequests ->
+                                authorizeRequests
+                                        .requestMatchers("/", "/index.html").permitAll()
+                                        .requestMatchers("/api/**").authenticated()
+                                        .requestMatchers("/admin/**").hasRole("ADMIN") // 일단 임시로..
+                                        .anyRequest().permitAll()
+                        );
+             
+                return http.build();
+        }
+
+        // CORS 설정
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() { 
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.addAllowedOriginPattern("*");
+                configuration.addAllowedMethod("*");
+                configuration.addAllowedHeader("*");
+                configuration.setAllowCredentials(true);
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
+}
